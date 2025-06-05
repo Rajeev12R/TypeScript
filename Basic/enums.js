6242e27a000311594338728653076cdc530754fd
Rajeev Ranjan <ranjan@Rajeevs-MacBook-Air-3.local>
2025-06-05 17:35:45 +0530

    enums

---
+++ Basic/enums.js
@@ -0,0 +1,12 @@
+var userrole;
+(function (userrole) {
+    userrole["ADMIN"] = "admin";
+    userrole["GUEST"] = "guest";
+    userrole["SUPER_ADMIN"] = "super_admin";
+})(userrole || (userrole = {}));
+var statuscode;
+(function (statuscode) {
+    statuscode["ABANDONED"] = "abandoned status code with 500";
+    statuscode["NOTFOUND"] = "not found status code 404";
+})(statuscode || (statuscode = {}));
+statuscode.NOTFOUND;
---
+++ Basic/enums.ts
@@ -0,0 +1,12 @@
+enum userrole {
+    ADMIN = "admin",
+    GUEST = "guest",
+    SUPER_ADMIN = "super_admin"
+}
+
+enum statuscode {
+    ABANDONED = "abandoned status code with 500",
+    NOTFOUND = "not found status code 404",
+}
+
+statuscode.NOTFOUND
