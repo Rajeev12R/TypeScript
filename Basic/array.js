d04d2580040d3d9bf6304e50cc595654f38ffa30
Rajeev Ranjan <ranjan@Rajeevs-MacBook-Air-3.local>
2025-06-10 10:14:07 +0530

    Array Basics

---
+++ Basic/array.js
@@ -0,0 +1,8 @@
+var arr = [1, 2, 3, "harry"];
+var arr2 = [1, 2, 3, { name: "harry" }];
+for (var i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+}
+for (var i = 0; i < arr2.length; i++) {
+    console.log(arr2[i]);
+}
---
+++ Basic/array.ts
@@ -0,0 +1,10 @@
+let arr = [1, 2, 3, "harry"];
+let arr2 = [1, 2, 3, { name: "harry" }];
+// let arr3: number[] = [1, 2, 3, 4, "rajeev"];
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i])
+}
+
+for (let i = 0; i < arr2.length; i++) {
+  console.log(arr2[i])
+}
