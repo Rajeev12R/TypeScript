fbeeffdbd59bc2cf563827cdd876e968f44a2d23
Rajeev Ranjan <ranjan@Rajeevs-MacBook-Air-3.local>
2025-06-10 10:17:39 +0530

    tuples

---
+++ Basic/tuples.js
@@ -0,0 +1,3 @@
+// tuples are fixed-length arrays with known types for each index
+var tup = [1, "Rajeev", true];
+console.log(tup);
---
+++ Basic/tuples.ts
@@ -0,0 +1,6 @@
+// tuples are fixed-length arrays with known types for each index
+
+let tup : [number, string, boolean] = [1, "Rajeev", true];
+
+console.log(tup);
+
